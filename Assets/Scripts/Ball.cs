54a0e9e5c17504341f7585ca1636fac445473a66
ChingDojoKitsune <ChingDojoKitsune@users.noreply.github.com>
2019-01-11 19:36:56 -0700

    plsplspls

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,7 +3,7 @@
 public class Ball : MonoBehaviour {
 
 	// config parameters
-	[SerializeField] Paddle paddle1; 
+	[SerializeField] Player1 paddle1; 
 	[SerializeField] float xPush = 2f;
 	[SerializeField] float yPush = 15f;
 	[SerializeField] AudioClip[] ballSounds;
