9c502d4bdc25198f9fb495519e4ef5a1e0552405
ChingDojoKitsune <jamesm12@learn.cssd.ab.ca>
2019-01-16 13:39:38 -0800

    rebase errors
    refactored start screen
    
    movement of character
    
    added BGM
    
    worked on level1
    
    test commit
    
    yikes
    
    reupdate
    
    gergerte
    
    dstfsjk
    
    fixed stuff
    
    dsfds
    
    fixed colliders
    
    save
    
    fwerer
    
    bvgv

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:Assets/Scripts/Player1.cs
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -57,3 +58,63 @@ void Update()
 }
 //just to push
 //test
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// configuration parameters
+	[SerializeField] float minX = 1f;
+	[SerializeField] float maxX = 16.63f;
+	[SerializeField] float screenWidthInUnits = 16.63f;
+
+	//cache refs
+	GameStatus theGameStatus;
+	Ball theBall;
+	
+	
+
+	// Use this for initialization
+	void Start () {
+		
+		theGameStatus = FindObjectOfType<GameStatus>();
+		theBall = FindObjectOfType<Ball>();
+	}
+	
+	// Update is called once per frame
+
+	public float MovementSpeed;
+ 
+void Update()
+{
+   Vector3 direction = CalculateDirection();
+   transform.Translate(direction * MovementSpeed * Time.deltaTime);
+}
+	public Vector3 CalculateDirection()
+{
+   Vector3 direction = Vector3.zero;
+   if (Input.GetKey(KeyCode.W))
+   {
+     direction.y += 1.0f;
+   }
+   if (Input.GetKey(KeyCode.A))
+   {
+     direction.x -= 1.0f;
+   }
+   if (Input.GetKey(KeyCode.S))
+   {
+     direction.y -= 1.0f;
+   }
+   if (Input.GetKey(KeyCode.D))
+   {
+     direction.x += 1.0f;
+   }
+   return direction.normalized;
+}
+
+
+}
+//just to push
+>>>>>>> 28ca959... test commit:Assets/Scripts/Paddle.cs
