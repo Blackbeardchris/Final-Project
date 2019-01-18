eb77339cd2f59f01c64044fef6c1d49024709aa5
Christopher Landry <christopherl10@learn.cssd.ab.ca>
2019-01-18 13:38:28 -0700

    yeet

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player1 : MonoBehaviour {
+/* 
+[SerializeField] int _attack = 6;
+[SerializeField] int _maximumHealth = 100;
+[SerializeField] int _int = 6;
+[SerializeField] int hitRange = 1;
+[SerializeField] int health = 100;
+*/
 
 	// configuration parameters
 	[SerializeField] float minX = 1f;
 	[SerializeField] float maxX = 16.63f;
 	[SerializeField] float screenWidthInUnits = 16.63f;
+
+
 
 	//cache refs
 	GameStatus theGameStatus;
@@ -20,13 +29,18 @@ public class Player1 : MonoBehaviour {
     public float runSpeed = 40f;
     public SpriteRenderer mySpriteRenderer;
 	
-
 	// Use this for initialization
 	void Start () {
-		
+/*   isClicked = false;
+     if (gameObject.GetComponent(Collider) == null)
+     {
+        gameObject.AddComponent(typeof(BoxCollider));
+     }
 		theGameStatus = FindObjectOfType<GameStatus>();
 		theBall = FindObjectOfType<Ball>();
+    */
 	}
+
 	
 	// Update is called once per frame
 
@@ -34,12 +48,40 @@ public class Player1 : MonoBehaviour {
  
 void Update()
 {
-   Vector3 direction = CalculateDirection();
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-   transform.Translate(direction * MovementSpeed * Time.deltaTime);
+  /* 
+  if (Input.GetKey(KeyCode.R)) {
+    Attack();
+  }
+  */
+  //Vector3 cool = basicAttack();
+  Vector3 direction = CalculateDirection();
+  horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+  animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+  transform.Translate(direction * MovementSpeed * Time.deltaTime);
 
 }
+/*
+void TakeDamage(int damageAmount)
+ {
+     health = health - damageAmount;
+ 
+     // We should also check if the health is still greater than 0 
+     // in order to determine whether enemy is still alive or not
+ 
+     if(health < 0)
+     {
+         // This enemy is supposed to be dead now.
+     }
+ }
+*/
+/* void Attack() {
+     if (Physics.Raycast(origin, forward, hitRange, out hit)) {
+        if (hit.transform.gameObject.tag == "Enemy") {
+            hit.transform.gameObject.SendMessage("TakeDamage", 30);
+        }
+    }
+}
+*/
 	public Vector3 CalculateDirection()
 {
    Vector3 direction = Vector3.zero;
